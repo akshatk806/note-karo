a91ebdfe5131d4c423f08c061f3cde68325c795a
akshatk806 <akshatk806@gmail.com>
2023-05-29 01:16:09 +0530

    toggle the color pallete when we click on Add button

--- src/Components/Sidebar/Siderbar.js
+++ src/Components/Sidebar/Siderbar.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Sidebar.css"
 
 const Siderbar = () => {
     const colors = ["#fe9b72", "#fec971", "#00d4fe", "#b693fd", "#e4ee91"]
 
+    // state variable for toggle the color pallete when we click on Add button
+    const [listOpen, setListOpen] = useState(false);   // state tells us whether the color pallete is open or not
+
   return (
     <div className="sidebar">
-        <img src="images/plus.png" alt="" />
-        <ul className="sidebar-list">
+        <img src="images/plus.png" alt="Add" onClick={()=>setListOpen(!listOpen)}/>
+        <ul className={ `sidebar-list ${listOpen ? "sidebar-list-active":""}` }>
             {
                 // generating the color dynamically
                 colors.map((color, index) => (
