46bbfd252fb870e93e6ee15f46b4ab022e6b815e
akshatk806 <akshatk806@gmail.com>
2023-05-29 00:13:54 +0530

    3. creating the custom scroll bar only for notes we don't want our App to scroll down

--- src/Components/NoteContainer/NoteContainer.js
+++ src/Components/NoteContainer/NoteContainer.js
@@ -1,14 +1,69 @@
 import React from 'react'
 import Note from '../Note/Note.js'
+import "./NoteContainer.css"
+
 const NoteContainer = () => {
   return (
-    <>
-        <Note note={{
-            text:"akshat",
-            time:"6:69 PM 28 May",
-            color:"pink"
-        }} />
-    </>
+    <div className="note-container">
+        <h2>Note Karo</h2>
+        <div className="note-container-notes custom-scroll">
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+            <Note note={{
+                text:"akshat",
+                time:"6:69 PM 28 May",
+                color:"pink"
+            }} />
+        </div>
+    </div>
   )
 }
 
