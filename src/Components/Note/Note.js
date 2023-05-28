458ac4480f67f5049bfbd480a7100c8773a39355
akshatk806 <akshatk806@gmail.com>
2023-05-29 04:32:22 +0530

    11. formatting the date

--- src/Components/Note/Note.js
+++ src/Components/Note/Note.js
@@ -11,12 +11,44 @@ const Note = (props) => {      // by using the props we dynamically added the no
     }
     */
 
+    const formatDate = (value) => {
+      // value is in milliseconds from EPOCH
+      if(!value) {
+        return ""
+      }
+      const date = new Date(value);
+      const monthNames = [
+        "Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+      ]
+      const days = [
+        "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"
+      ]
+
+      let hours = date.getHours();
+
+      let AmPm = hours > 12 ? "PM" : "AM"
+
+      hours = hours ? hours : "12"
+      hours = hours > 12 ? 24-hours : hours
+
+      let min = date.getMinutes();
+      min = min < 10 ? "0"+min : min;
+
+      let day = date.getDate();
+
+      let month = monthNames[date.getMonth()];
+
+      let year = date.getFullYear();  
+      
+      return `${hours}:${min} ${AmPm}, ${day} ${month} ${year}`;
+    }
+
   return (
     <>
         <div className="note" style={{backgroundColor:props.note.color}}>
             <textarea className="note-text" defaultValue={props.note.text}/>
             <div className="note-footer">
-              <p>{props.note.time}</p>
+              <p>{formatDate(props.note.time)}</p>
               <img src="/images/delete.svg" 
                 alt="Delete Note" 
                 onClick={()=>props.deleteNote(props.note.id)}
